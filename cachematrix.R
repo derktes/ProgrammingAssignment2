9e26e95c0c7960685c744b832052dcff45b71430
noname <nomail@nomail>
2015-10-23 22:34:18 +0800

    implement stubs

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,42 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverseMatrix <- NULL
+    
+    set <- function(y) {
+        x <<- y
+        inverseMatrix <<- NULL
+    }
+    
+    get <- function() {
+        x
+    }
+    
+    setInverse <- function(inv) {
+        inverseMatrix <<- inv
+    }
+    
+    getInverse <- function() {
+        inverseMatrix
+    }
+    
+    list(set = set, get = get, 
+         setInverse = setInverse, getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inverseMatrix <- x$getInverse()
+    
+    if (!is.null(inverseMatrix)) {
+        message("getting cached inverse matrix")
+        return(inverseMatrix)
+    }
+    
+    inverseMatrix <- solve(x$get())
+    x$setInverse(inverseMatrix)
+    inverseMatrix
 }
